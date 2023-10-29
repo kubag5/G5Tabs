4d6c4c09ba11fa1e2130ea6834bd983de8203c72
kubag5 <79789131+kubag5@users.noreply.github.com>
2023-10-29 17:19:15 +0100

    0.0.4

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # G5Tabs
-G5Tabs is a simple project for creating your own subpages
+* [do czego służy aplikacja?](#do-czego-służy-aplikacja)
+* [jak będą działać zakładki?](#jak-będą-działać-zakładki)
+* [kategorie zakładek](#kategorie-zakładek)
+
+## do czego służy aplikacja?
+Aplikacja będzie umożliwiać tworzenie użytkownikowi własnych zakładek.
+
+## jak będą działać zakładki?
+zakładki będą podstronami strony które będą zawierały różne informacje w zależności od typu zakładki. czyli zakładki to będą strony informacyjne utworzone przez użytkowników.
+
+użytkownik będzie mógł wybrać czy zakładka ma być publiczna czy prywatna.
+
+## Kategorie zakładek: 
+<details>
+<summary>Linia czasu</summary>
+użytkownik w lini czasu mógłby  dodawać co się stało lub stanie w danym dniu. a następnie strona by generowała linie na środku ekranu a na górze lub dole generowała by pojemniki z informacjami umieszczonymi przez użytkownika.
+</details>
+<details>
+<summary>Plan</summary>
+użytkownik w planie może dodawać jakie ma plany na przyszłość. strona będzie wyświetlała liste z odliczaniem do każdego poszczególnego planu na przyszłość.
+</details>
+<br/>
+nowe kategorie zakładek będą wymyślane z czasem. 
--- index.php
+++ index.php
@@ -23,33 +23,7 @@
              <div style="clear: both"></div>
          </div>
          </div>
-         <div id="content">
-
-            <div id="loginpanel">  
-            <h1>Witaj w G5Tabs</h1>
-            <br/>
-           
-                <form id="loginform" style="display: none;"> 
-                <p>Oto panel logowania do G5Tabs:</p><br/>
-                    <label for="login">Login: </label><br/><br/>
-                    <input type="text" name="login" id="login" class="LRGI"> <br/><br/>
-                    <label for="pass">Hasło: </label><br/><br/>
-                    <input type="password" name="pass" id="password" class="LRGI"><br/><br/><br/>
-                    <input type="submit" value="Zaloguj się" class="LRGBI">
-                </form>
-                <form id="registerform" style="display: none;"> 
-                <p>Oto panel rejestracji do G5Tabs:</p><br/>
-                    <label for="login">Login: </label><br/><br/>
-                    <input type="text" name="login" id="login" class="LRGI"> <br/><br/>
-                    <label for="pass">Hasło: </label><br/><br/>
-                    <input type="password" name="pass" id="password" class="LRGI"><br/><br/>
-                    <label for="pass2">Powtórz Hasło: </label><br/><br/>
-                    <input type="password" name="pass2" id="password" class="LRGI"><br/><br/><br/>
-                    <input type="submit" value="Zarejestruj się" class="LRGBI">
-                </form><br/><br/>
-                <button onclick="changeLRG();" id="LRGBT" class="LRGBI" >Chce sie zarejestrować</button>
-            </div>
-            
+         <div id="content">           
             <?php 
 
             function check() {
@@ -58,7 +32,30 @@
 
             function notlogged() {
                 echo "
-              
+                <div id='loginpanel'>  
+                <h1>Witaj w G5Tabs</h1>
+                <br/>
+               
+                    <form id='loginform' style='display: none;'> 
+                    <p>Oto panel logowania do G5Tabs:</p><br/>
+                        <label for='login'>Login: </label><br/><br/>
+                        <input type='text' name='login' id='login' class='LRGI'> <br/><br/>
+                        <label for='pass'>Hasło: </label><br/><br/>
+                        <input type='password' name='pass' id='password' class='LRGI'><br/><br/><br/>
+                        <input type='submit' value='Zaloguj się' class='LRGBI'>
+                    </form>
+                    <form id='registerform' style='display: none;'> 
+                    <p>Oto panel rejestracji do G5Tabs:</p><br/>
+                        <label for='login'>Login: </label><br/><br/>
+                        <input type='text' name='login' id='login' class='LRGI'> <br/><br/>
+                        <label for='pass'>Hasło: </label><br/><br/>
+                        <input type='password' name='pass' id='password' class='LRGI'><br/><br/>
+                        <label for='pass2'>Powtórz Hasło: </label><br/><br/>
+                        <input type='password' name='pass2' id='password' class='LRGI'><br/><br/><br/>
+                        <input type='submit' value='Zarejestruj się' class='LRGBI'>
+                    </form><br/><br/>
+                    <button onclick='changeLRG();' id='LRGBT' class='LRGBI' >Chce sie zarejestrować</button>
+                </div>
                 ";
                 
             }
